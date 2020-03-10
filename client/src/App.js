fcf1098fa25960e3256515e87e8486e0382eddbf
ArjunMohan008 <mohanarjun.juner@gmail.com>
2020-03-10 14:39:59 +0530

    initial client setup

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import Navbar from './components/Navbar';
+import Landing from './components/Landing';
+import './App.css';
+
+const App = () => (
+  <Fragment>
+    <Navbar />
+    <Landing />
+  </Fragment>
+);
+export default App;

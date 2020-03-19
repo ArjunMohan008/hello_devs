e624b332f5f1c5794d43667ebd21cba326817758
ArjunMohan008 <mohanarjun.juner@gmail.com>
2020-03-19 09:59:12 +0530

    Profile actions done

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-
 import { GET_PROFILE, PROFILE_ERROR } from './types';
 
 // Get current users profile
@@ -13,6 +12,45 @@ export const getCurrentProfile = () => async dispatch => {
       payload: res.data
     });
   } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Create or update profile
+export const createProfile = (
+  formData,
+  history,
+  edit = false
+) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    const res = await axios.post('/api/profile', formData, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'));
+
+    if (!edit) {
+      history.push('/dashboard');
+    }
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
     dispatch({
       type: PROFILE_ERROR,
       payload: { msg: err.response.statusText, status: err.response.status }

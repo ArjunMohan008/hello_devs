5bc7646a13a275525cecd64ad81dd5d2e1291af7
ArjunMohan008 <mohanarjun.juner@gmail.com>
2020-03-19 10:57:32 +0530

    Edit profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import DashboardAction from './DashboardAction';
 import { getCurrentProfile } from '../../actions/profile';
 
 const Dashboard = ({
@@ -24,7 +25,9 @@ const Dashboard = ({
         Welcome {user && user.name}
       </p>
       {profile !== null ? (
-        <Fragment>has</Fragment>
+        <Fragment>
+          <DashboardAction />
+        </Fragment>
       ) : (
         <Fragment>
           <p>You have not yet setup a profile, please add some info</p>

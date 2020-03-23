29c0b2bc81f527a1788c2b1ef1f826afc98ed7a3
ArjunMohan008 <mohanarjun.juner@gmail.com>
2020-03-23 18:24:51 +0530

    Deploy configurations

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const path = require('path;');
+const path = require('path');
 
 const app = express();
 
@@ -16,13 +16,13 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 
-// server static assets in production
+// Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 

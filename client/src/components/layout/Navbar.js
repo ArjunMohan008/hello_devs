d87c982222f31efcd0d92c65eb86303a1f835efa
arjunmohan008 <mohanarjun.juner@gmail.com>
2022-12-26 14:15:16 +0530

    feat: name change to hello devs

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -48,7 +48,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     <nav className='navbar bg-dark'>
       <h1>
         <Link to='/'>
-          <i className='fas fa-code'></i> DevConnector
+          <i className='fas fa-code'></i> HelloDevs
         </Link>
       </h1>
       {!loading && (
